dc81a1636e8105473561971f3acdf5fe0b623105
Arjan Scherpenisse <arjan@scherpenisse.net>
2014-05-06 21:35:05 +0200

    ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,5 +1,9 @@
 
 void shoot() {
+  digitalWrite(13, HIGH);
+delay(200);
+digitalWrite(13, LOW);
+
   // warm up motor
   digitalWrite(8, HIGH);
   delay(4000);
@@ -11,22 +15,30 @@ void shoot() {
   delay(1000);
   
   digitalWrite(8, LOW);
+  flush();
 }
 
 
 void setup() {                
   pinMode(8, OUTPUT);     
-  pinMode(9, OUTPUT);     
+  pinMode(9, OUTPUT);    
+  pinMode(13, OUTPUT); 
   Serial.begin(19200);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     int ch = Serial.read();
+    flush();
+    
     if (ch == 1) {
       shoot();
     }
   }
+}
+
+void flush() {
+  while (Serial.available()>0) Serial.read();
 }
 
 
